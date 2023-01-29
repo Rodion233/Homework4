dc33ae13b3424e1cbf6b28dbe9017ae67c52a77b
Rodion23 <rodion23@mail.ru>
2023-01-29 13:43:03 +0700

    Add HW3

--- HW3/Program.cs
+++ HW3/Program.cs
@@ -2,3 +2,23 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]
 */
+int[] GenerateArray(int len = 8)
+{
+    int[] array = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 200);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        System.Console.Write($"{item}\t");
+    }
+    System.Console.WriteLine();
+}
+int[] array = GenerateArray();
+PrintArray(array);
